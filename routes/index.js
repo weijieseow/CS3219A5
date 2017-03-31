fe3f40b5042e320c665555e59e4005dc2264b3dd
Seow Weijie <sweijie94@gmail.com>
2017-03-31 15:11:43 +0800

    Able to clone the repo url successfully

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var clone = require('git-clone');
+var shell = require('shelljs');
 
 var urlEncodoedParser = bodyParser.urlencoded({ extended: false });
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index');
+	res.render('index');
 });
 
 router.post('/url', urlEncodoedParser, function(req, res){
-   // req.body object has your form values
-   console.log(req.body.url);
-   res.render('index');
+   console.log(req.body.url); // url of repo
+
+   shell.cd(__dirname + "/..");
+   shell.exec('rm -rf repo');
+
+   clone(req.body.url, __dirname + "/../repo");
+   res.redirect('/');
 });
 
 module.exports = router;
