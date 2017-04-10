a7306bc54795cea73f5d8cf0413679076bb4b89f
See Loo Jane <muigek34@yahoo.com.sg>
2017-04-10 18:48:08 +0800

    Add stub horizontal bar chart code to file.ejs Linked up number of commits json data with bar chart

--- routes/file.js
+++ routes/file.js
@@ -1,33 +1,158 @@
 var express = require('express');
 var router = express.Router();
-var shell = require('shelljs');
 var git = require('simple-git');
 var fs = require('fs');
+var bodyParser = require('body-parser');
+var postprocessor = require('../data-controller/postprocessor.js');
 
+var urlEncodoedParser = bodyParser.urlencoded({ extended: false });
+
+// Define the variables that should be passed on to HTML here
+var dArrayOfCommits = []
+var dArrayOfCommitNumber = []
+var dArrayOfAddDelete = [] 
 
 router.get('/', function(req, res) {
 
-	res.render('file');
+	// Render Page with empty / default variablesa
+	res.render('file', {
+		filepath: "Showing stats for file: " + req.session.filepath || "No file specified.",
+		commitsArr: dArrayOfCommits || [],
+		datasetNumCommits: dArrayOfCommitNumber || [],
+		datasetAddDelete: dArrayOfAddDelete || [],
+	});
 
+	// DEBUG
 	console.log("Current Repo: " + req.session.repo)
 });
 
 router.post('/filepath', urlEncodoedParser, function(req, res) {
-   console.log(req.body.filepath); // Filepath to be inspected
    
-   req.session.repo = req.body.url; // save the repo in current session
+   	var dataDict = {}
+	var newStatsObject = {
+		numCommits: 0,
+		addition: 0,
+		deletion: 0,
+	}
 
-   shell.cd(__dirname + "/..");
-   shell.exec('rm -rf repo');
+	var dAuthorsArr = [];
+	var dCommitMsgArr = [];
+	var dAdditionsArr = [];
+	var dDeletionsArr = [];
 
-   clone(req.body.url, __dirname + "/../repo", function() {
-   	//process data here using the cloned repo in {working directory}/repo
-   	//store the data as csv files in public/data
-   	
-   	//git(__dirname + "/../repo").status((err, summary) => console.log(summary));
+   let filepath = req.body.filepath
+   console.log(filepath); // Filepath to be inspected
 
-   	res.redirect('/overview');
-   });
+   // Pull data for part D
+	git(__dirname + "/../repo").raw([
+	'log',
+	'--follow',
+	'--numstat',
+	'--pretty=format:"%aN%n%s"',
+	filepath,
+	], (err, result) => {
+
+		//console.log(result)
+
+		// TODO: Check for null result 
+
+		var lines = result.split('\n') 
+		for(var i = 0; i < lines.length; i++){
+			
+			let line = lines[i]
+			// Every multiple of 4 (starting from 0) is the NAME, without first char (")
+			if (i%4 == 0) {
+				dAuthorsArr.push(line.substring(1))
+
+			// This line is  commit msg without last char (")
+			} else if (i%4 == 1) {
+				dCommitMsgArr.push(line.slice(0, -1)) 
+			
+			// this line is addition and deletions
+			} else if (i%4 == 2) {
+				let delimitedByTab = line.split('\t')
+				dAdditionsArr.push(parseInt(delimitedByTab[0]))
+				dDeletionsArr.push(parseInt(delimitedByTab[1]))  				
+			}
+
+		}
+
+		// Combine arrays into an array of JSON objects repsenting a commit
+		for (var i = 0; i < dAuthorsArr.length; i++) {
+			var dObject = {
+				author: dAuthorsArr[i],
+				addition: dAdditionsArr[i],
+				deletion: dDeletionsArr[i],
+				msg: dCommitMsgArr[i],
+			}
+
+			dArrayOfCommits.push(dObject)
+		}
+
+		//console.log(dArrayOfCommits);
+
+		// Go through every commit and consolidate stats per author
+		for (var i = 0; i < dArrayOfCommits.length; i++) {
+			var commit = dArrayOfCommits[i]
+			var author = commit.author
+			
+			// If author is not in dictionary as a key, init new entry
+			if (!dataDict.hasOwnProperty(author)) {
+				dataDict[author] = newStatsObject
+			} 
+
+			dataDict[author].numCommits += 1
+			dataDict[author].addition += commit.addition
+			dataDict[author].deletion += commit.deletion
+		}
+
+		// Iterate through dictionary to create the respective datasets (array of JSONs) needed for d3
+
+		// Get list of unique keys in dict
+		var uniqueAuthors = [];
+		for (var author in dataDict) {
+			if (dataDict.hasOwnProperty(author)) {
+				uniqueAuthors.push(author);
+			}
+		}
+
+		// Get the stats JSON object for every key 
+		for (var i = 0; i < uniqueAuthors.length; i++) {
+			var currAuthor = uniqueAuthors[i]
+			
+			var numCommitsStat = {
+				label: currAuthor,
+				value: dataDict[currAuthor].numCommits
+			}
+
+			var addDeleteStat = {
+				author: currAuthor,
+				addition: dataDict[currAuthor].addition,
+				deletion: dataDict[currAuthor].deletion,
+			}
+
+			dArrayOfCommitNumber.push(numCommitsStat)
+			dArrayOfAddDelete.push(addDeleteStat)
+		}
+
+		req.session.filepathData = {
+			commits: dArrayOfCommits,
+			numCommits: dArrayOfCommitNumber,
+			addDelete: dArrayOfAddDelete, 
+		}
+		
+	});
+
+	// Render page now with the data
+	res.render('file', {
+		filepath: "Showing stats for file: " + req.session.filepath || "No file specified.",
+		commitsArr: dArrayOfCommits || [],
+		datasetNumCommits: dArrayOfCommitNumber || [],
+		datasetAddDelete: dArrayOfAddDelete || [],
+	});
+
+
+
 });
 
 module.exports = router;
