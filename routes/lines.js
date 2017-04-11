c524f807e88aa3d32043579e94e5bd3839efb052
roll <quek.ruoling@gmail.com>
2017-04-11 13:53:00 +0800

    Wrap commands in promise and display data

--- routes/lines.js
+++ routes/lines.js
@@ -7,8 +7,8 @@ var fs = require('fs');
 
 router.get('/', function(req, res) {
 
-  // git grep command to get all non-binary files in the latest version
-  // for more accurate line attribution
+  // git grep command to get all non-binary files
+  // checking non-binary files will provide more accurate line attribution
   git(__dirname + "/../repo").raw([
     'grep',
     '-I', '-l',
@@ -19,46 +19,55 @@ router.get('/', function(req, res) {
         res.render('lines');
         return;
       }
+    
       // result is a string of filenames separated by newline
       // process into an array of filenames for easier manipulation
       var fileNames = result.split("\n");
 
-      // compute the number of lines by each author per file
-      var count = {};
-      var data;
-      // git blame command for each file to get line attribution
-      fileNames.forEach(file => {
-        git(__dirname + '/../repo').raw([
-          'blame',
-          '--line-porcelain',
-          file
-        ], (err, result) => {
-          if (err) {
-            console.log('blame failed: ' + err);
-          } else {
+      // get line attribution using git blame command
+      // wrap each command in a Promise as they are executed asynchronously
+      var promises = fileNames.map(file => {
+        return new Promise((resolve, reject) => {
+          git(__dirname + '/../repo').raw([
+            'blame',
+            '--line-porcelain',
+            file
+          ], (err, result) => {
+            if (err) {
+              return resolve([]); // return an empty array
+            }
+            
             // get the commit author for each line
-            var authors = result.split("\n").filter(
-              text => text.startsWith('author ')).map(text => text.substr(7));
-            // count the occurrences of commit authors
-            authors.forEach(e => {
-              count[e] = (count[e] || 0) + 1;
-            });
-            
-            // convert the object to array
-            data = Object.keys(count).map(author => {
-              return {
-                author: author,
-                count: count[author] 
-              };
-            });
-            
-            // console.log(data);
-          }
+            var authors = result.split("\n")
+                          .filter(text => text.startsWith('author '))
+                          .map(text => text.substr(7));
+            resolve(authors);
+          });
         });
       });
-      res.render('lines', {
-        data: data
+    
+      // execute all commands, then compute the number of lines by each author per file
+      Promise.all(promises).then(files => {
+        var count = {};
+        files.forEach(file => {
+          file.forEach(author => {
+            count[author] = (count[author] || 0) + 1;
+          });
+        });
+        
+        // convert the object to array
+        data = Object.keys(count).map(author => {
+          return {
+            author: author,
+            count: count[author] 
+          };
+        });
+        console.log(data);
+        res.render('lines', {
+          data: data
+        });
       });
+      
     });  
 });
 
